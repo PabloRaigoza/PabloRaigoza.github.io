1fcbf4421e3d098d3a4345c4ea3668187ca07130
Pablo Raigoza <41972903+PabloRaigoza@users.noreply.github.com>
2022-01-17 16:11:55 -0800

    Update script.js

--- testFolder/script.js
+++ testFolder/script.js
@@ -5,17 +5,17 @@ class Point {
     }
 }
 
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js").then(registration => {
-        console.log("SW Registered!");
-        console.log(registration);
-    }).catch(error => {
-        console.log("failure");
-        console.log(error);
-    });
-} else {
-    console.log("application not supported");
-}
+// if ("serviceWorker" in navigator) {
+//     navigator.serviceWorker.register("sw.js").then(registration => {
+//         console.log("SW Registered!");
+//         console.log(registration);
+//     }).catch(error => {
+//         console.log("failure");
+//         console.log(error);
+//     });
+// } else {
+//     console.log("application not supported");
+// }
 
 
 
